46624d3de8ee3fbd43db2a59ac738b0f68940532
satria <satriawahyu004@gmail.com>
2020-05-05 13:24:24 +0700

    pembuatan class anak(derived class)

---
+++ LaserJet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymorphism
+{
+    class LaserJet : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("LaserJet display dimension : 12*12 ");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("LaserJet printer printing.......");
+        }
+    }
+}
