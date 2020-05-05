96853f47d05508821e896722c6e9eb1d1189d9b0
satria <satriawahyu004@gmail.com>
2020-05-05 13:12:13 +0700

    Pembuatan class induk/base class

---
+++ PrinterWindows.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymorphism
+{
+    class PrinterWindows
+    {
+        public string merk { get; set; }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("MERK display dimension : UKURAN");
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("MERK Printer printing.......");
+        }
+    }
+}
