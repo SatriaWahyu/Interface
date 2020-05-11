a9764436b933de3f4314166eee713b121d3ffadb
satria <satriawahyu004@gmail.com>
2020-05-11 18:38:57 +0700

    Pembuatan Interface

---
+++ IPrinterWindows.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismWithInterface
+{
+    interface IPrinterWindows
+    {
+        //methode abstract
+        void Show(Tampilkan tampil);
+        void Print(Mencetak cetak);
+    }
+}
