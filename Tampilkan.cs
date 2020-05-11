fe32621d0e3ce9972fe36f0efbe78d9be90f85c8
satria <satriawahyu004@gmail.com>
2020-05-11 19:00:24 +0700

    Class Tampilkan dan Mencetak

---
+++ Tampilkan.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismWithInterface
+{
+    class Tampilkan
+    {
+        public string merk { get; set; }
+    }
+}
